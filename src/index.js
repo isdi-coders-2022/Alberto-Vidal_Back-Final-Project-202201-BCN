a6fd3d1e516bde06d9bc2e7a04cce286893d8d98
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-03-07 16:44:12 +0000

    refactored into a function raiseServer

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 require("dotenv").config();
-const debug = require("debug")("PS_api:root:");
 const express = require("express");
-const chalk = require("chalk");
+const raiseServer = require("./server");
 
 const app = express();
 const port = process.env.PORT || 5555;
 
-const server = app.listen(port, () => {
-  debug(chalk.bgGreen.black(`server listening at http://localhost:${port}`));
-});
-
-server.on("error", () => {
-  debug(chalk.red("error raising server"));
-});
+(async () => {
+  await raiseServer(app, port);
+})();
 
 app.use((req, res) => {
   res.json({ hello: "world" });
