636d316249ba114672b6057eb881aa5c92834c30
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-03-07 18:20:58 +0000

    cerated server error middleware

--- src/server/middlewares/errors.js
+++ src/server/middlewares/errors.js
@@ -2,4 +2,12 @@ const notFound = (req, res) => {
   res.status(404).json({ error: "resource not found" });
 };
 
+// eslint-disable-next-line no-unused-vars
+const serverError = (err, req, res, next) => {
+  const errorStatus = err.status ?? 500;
+  const errorMessage = err.message ?? "internal server error";
+
+  res.status(errorStatus).json({ error: errorMessage });
+};
+
 module.exports = { notFound };
