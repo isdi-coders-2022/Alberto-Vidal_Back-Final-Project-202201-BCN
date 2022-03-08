d0fc11865d82d2e5b5419e02432ebbd8b8ca6734
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-03-08 09:41:50 +0000

    crated userSchema

---
+++ src/database/schemas/UserSchema.js
@@ -0,0 +1,14 @@
+const { Schema } = require("mongoose");
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  name: { type: String, required: false },
+  password: { type: String, required: true },
+  posts: { type: Schema.Types.ObjectId, ref: "project" },
+});
+
+module.exports = UserSchema;
