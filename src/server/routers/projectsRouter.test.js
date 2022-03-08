662d9a0c1fb8ca8059cc6d930afb3361f934a78a
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-03-08 13:57:29 +0000

    tested get all projects route

--- src/server/routers/projectsRouter.test.js
+++ src/server/routers/projectsRouter.test.js
@@ -4,6 +4,7 @@ const { MongoMemoryServer } = require("mongodb-memory-server");
 const mongoose = require("mongoose");
 const connectDB = require("../../database");
 const Project = require("../../database/models/Project");
+const app = require("..");
 
 let mongod;
 beforeAll(async () => {
@@ -12,25 +13,30 @@ beforeAll(async () => {
   connectDB(uri);
 });
 
-beforeEach(async () => {
-  await Project.create({
-    author: ObjectID(),
+const projectsDatabase = [
+  {
+    author: ObjectID("62275e1f60e1a2984ec63453"),
     likes: 3,
     preview: "image.png",
     links: {
       production: "vercel.app/project1",
       repo: "github.com/project1",
     },
-  });
-  await Project.create({
-    author: ObjectID(),
+  },
+  {
+    author: ObjectID("62275e1f60e1a2984ec63454"),
     likes: 4,
     preview: "image.png",
     links: {
       production: "vercel.app/project1",
       repo: "github.com/project1",
     },
-  });
+  },
+];
+
+beforeEach(async () => {
+  await Project.create(projectsDatabase[0]);
+  await Project.create(projectsDatabase[1]);
 });
 
 afterEach(async () => {
@@ -44,6 +50,12 @@ afterAll(async () => {
 
 describe("Given a projects router", () => {
   describe("When it receives a request at /projects/all", () => {
-    test("Then it should respond with all the projects in the database", () => {});
+    test("Then it should respond with all the projects in the database", async () => {
+      const { body } = await request(app).get("/projects/all");
+
+      expect(body).toHaveLength(projectsDatabase.length);
+      expect(body[0].author).toEqual(projectsDatabase[0].author.toHexString());
+      expect(body[1].author).toEqual(projectsDatabase[1].author.toHexString());
+    });
   });
 });
