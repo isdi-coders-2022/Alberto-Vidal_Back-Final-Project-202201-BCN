a6673d6fe245c7f19e2265e31d9e7e75d6721d11
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-03-11 17:57:32 +0000

    created delete project controller

--- src/server/routers/projectsRouter.js
+++ src/server/routers/projectsRouter.js
@@ -4,5 +4,6 @@ const { getAllProjects } = require("../controllers/projectsControllers");
 const router = express.Router();
 
 router.get("/all", getAllProjects);
+router.delete("/delete/:id");
 
 module.exports = router;
