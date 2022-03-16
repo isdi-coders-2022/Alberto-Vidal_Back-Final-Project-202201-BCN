5e73de3efd14a703ffc2b9b1b813c619d218b354
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-03-16 19:41:02 +0100

    fixed register validation

--- src/server/middlewares/validators/userValidators.js
+++ src/server/middlewares/validators/userValidators.js
@@ -12,6 +12,7 @@ const registerValidator = {
     username: Joi.string().required(),
     password: Joi.string().required(),
     name: Joi.string().required(),
+    avatar: Joi.string().required(),
   }),
 };
 
