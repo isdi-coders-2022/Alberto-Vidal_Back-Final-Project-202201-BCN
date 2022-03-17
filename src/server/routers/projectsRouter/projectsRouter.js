b7f98e24c666dd00b232974c8d74e097fec342ef
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-03-17 20:03:10 +0100

    created update project controller

--- src/server/routers/projectsRouter/projectsRouter.js
+++ src/server/routers/projectsRouter/projectsRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { validate } = require("express-validation");
+const Project = require("../../../database/models/Project");
 const {
   getAllProjects,
   deleteProject,
@@ -14,5 +15,23 @@ const router = express.Router();
 router.get("/all", getAllProjects);
 router.delete("/delete/:id", deleteProject);
 router.post("/new", validate(createProjectValidator), createNewProject);
+router.put("/edit", async (req, res, next) => {
+  const projectToUpdate = req.body;
+  try {
+    const updatedProject = await Project.findByIdAndUpdate(projectToUpdate.id, {
+      ...projectToUpdate,
+      author: projectToUpdate.author.id,
+    });
+    const populatedUpdatedProject = await updatedProject.populate(
+      "author",
+      "username avatar id"
+    );
+
+    res.status(201).json(populatedUpdatedProject);
+  } catch (error) {
+    error.message = "error creating project";
+    next(error);
+  }
+});
 
 module.exports = router;
