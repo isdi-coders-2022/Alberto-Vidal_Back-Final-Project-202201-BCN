d390b414a7e4867e3068ee6ca4e7fe13a2326767
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-03-18 15:28:15 +0100

    validated edit project request

--- src/server/middlewares/validators/projectValidators.js
+++ src/server/middlewares/validators/projectValidators.js
@@ -9,4 +9,19 @@ const createProjectValidator = {
   }),
 };
 
-module.exports = { createProjectValidator };
+const editProjectValidator = {
+  body: Joi.object({
+    author: Joi.object({
+      username: Joi.string().required(),
+      id: Joi.string().required(),
+      avatar: Joi.string().required(),
+    }).required(),
+    preview: Joi.string().required(),
+    production: Joi.string(),
+    repo: Joi.string().required(),
+    id: Joi.string().required(),
+    likes: Joi.number().required(),
+  }),
+};
+
+module.exports = { createProjectValidator, editProjectValidator };
--- src/server/routers/projectsRouter/projectsRouter.js
+++ src/server/routers/projectsRouter/projectsRouter.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const { validate } = require("express-validation");
-const Project = require("../../../database/models/Project");
 const {
   getAllProjects,
   deleteProject,
   createNewProject,
+  editProject,
 } = require("../../controllers/projectsControllers/projectsControllers");
 const {
   createProjectValidator,
+  editProjectValidator,
 } = require("../../middlewares/validators/projectValidators");
 
 const router = express.Router();
@@ -15,29 +16,6 @@ const router = express.Router();
 router.get("/all", getAllProjects);
 router.delete("/delete/:id", deleteProject);
 router.post("/new", validate(createProjectValidator), createNewProject);
-router.put("/edit", async (req, res, next) => {
-  const projectToUpdate = req.body;
-
-  try {
-    const updatedProject = await Project.findByIdAndUpdate(
-      projectToUpdate.id,
-      {
-        ...projectToUpdate,
-        author: projectToUpdate.author.id,
-      },
-      { new: true }
-    );
-
-    const populatedUpdatedProject = await updatedProject.populate(
-      "author",
-      "username avatar id"
-    );
-
-    res.status(200).json(populatedUpdatedProject);
-  } catch (error) {
-    error.message = "error updating project";
-    next(error);
-  }
-});
+router.put("/edit", validate(editProjectValidator), editProject);
 
 module.exports = router;
