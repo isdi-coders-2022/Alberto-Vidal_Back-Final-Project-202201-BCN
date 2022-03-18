b254a26fc092cc67811d3631298212b076d8d1de
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-03-18 15:17:02 +0100

    tested edit project router

--- src/server/routers/projectsRouter/projectsRouter.js
+++ src/server/routers/projectsRouter/projectsRouter.js
@@ -17,19 +17,25 @@ router.delete("/delete/:id", deleteProject);
 router.post("/new", validate(createProjectValidator), createNewProject);
 router.put("/edit", async (req, res, next) => {
   const projectToUpdate = req.body;
+
   try {
-    const updatedProject = await Project.findByIdAndUpdate(projectToUpdate.id, {
-      ...projectToUpdate,
-      author: projectToUpdate.author.id,
-    });
+    const updatedProject = await Project.findByIdAndUpdate(
+      projectToUpdate.id,
+      {
+        ...projectToUpdate,
+        author: projectToUpdate.author.id,
+      },
+      { new: true }
+    );
+
     const populatedUpdatedProject = await updatedProject.populate(
       "author",
       "username avatar id"
     );
 
-    res.status(201).json(populatedUpdatedProject);
+    res.status(200).json(populatedUpdatedProject);
   } catch (error) {
-    error.message = "error creating project";
+    error.message = "error updating project";
     next(error);
   }
 });
